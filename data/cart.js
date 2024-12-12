cc8bd002dc2187e16e7625e8eccd90a3a71fc9ec
G_Krishnan_Naidu <2140gokulraj@gmail.com>
2024-12-12 21:18:00 +0530

    Refactor checkout page and update cart data structure. Removed detailed order summary from checkout.html and added a reference to checkout.js. Updated cart.js to initialize the cart with product IDs and quantities, enhancing the cart management functionality.

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,3 @@
-import { products } from "./products.js";
-
 export let cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
@@ -25,5 +23,4 @@ export function addToCart(productId) {
             quantity: 1
         });
     }
-    console.log('Cart:', cart);
-}
+} 
